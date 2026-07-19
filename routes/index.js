3d74f4bd4c1534dbea6c4d33fb1b3b6a08facb29
Ivan <aniv.ing.ec@gmail.com>
2026-07-18 22:54:15 -0500

    Description of the Website

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,13 @@ router.use("/employees", employeeRoutes);
 router.use("/accesslogs", accessLogRouters);
 
 router.get("/", (req, res) => {
-    res.send("RFID Access API");
+    res.status(200).json({
+        title: "RFID Employee Access Management API",
+        description: "RESTful API for managing employees and RFID access logs.",
+        course: "CSE341 - Web Services",
+        version: "1.0.0",
+        documentation: "/api-docs"
+    });
 });
 
 module.exports = router;
