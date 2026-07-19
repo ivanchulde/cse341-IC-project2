33847299cd6debff9fb153dc91373a5b92fff77d
Ivan <aniv.ing.ec@gmail.com>
2026-07-18 22:28:49 -0500

    Swagger Update 5.0

--- swagger.js
+++ swagger.js
@@ -4,12 +4,13 @@ const doc = {
   info: {
     title: "CSE341 Project 2 API",
     description: "API for Access Logs Management",
+    version: "1.0.0"
   },
-  host: "cse341-ic-project2.onrender.com/",
-  schemes: ["https"],
+  host: "cse341-ic-project2.onrender.com",
+  schemes: ["https"]
 };
 
 const outputFile = "./swagger-output.json";
-const endpointsFiles = ["./routes/index.js"];
+const endpointsFiles = ["./server.js"];
 
 swaggerAutogen(outputFile, endpointsFiles, doc);
